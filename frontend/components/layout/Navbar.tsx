0e4c440e8178b21a3e9acd66fa740fb8e29931f9
Eduard <eduard_king_anterola@dlsl.edu.ph>
2026-01-16 13:21:02 +0800

    feat: add homepage placeholder

--- frontend/components/layout/Navbar.tsx
+++ frontend/components/layout/Navbar.tsx
@@ -2,6 +2,7 @@
 
 import React, { useState, useEffect } from 'react'
 import Link from 'next/link'
+// import HomePage from '';
 
 export default function Navbar() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
