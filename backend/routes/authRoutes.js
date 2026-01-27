c99b4e18f2e365df0f99cdd12d45518a43c62530
Eduard <eduard_king_anterola@dlsl.edu.ph>
2026-01-27 14:39:01 +0800

    refactor: remove duplicate files

--- backend/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { requireAuth } = require('../middleware/auth');
-const { me } = require('../controllers/authController');
-
-const router = express.Router();
-
-router.get('/me', requireAuth, me);
-
-module.exports = router;
